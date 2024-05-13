95a6d50f2cd3cdb366adc1f1db9789242a92dd05
sof07 <64215063+sof07@users.noreply.github.com>
2024-05-13 22:31:15 +0300

    Добавил в админа user_id, подправил CRUD админа

--- app/crud/admin.py
+++ app/crud/admin.py
@@ -1,30 +1,31 @@
 from models.admin import Admin
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.core.db import AsyncSessionLocal
 
 
-class CRUDAdmin():
-
+class CRUDAdmin:
     def __init__(self, model):
         self.model = model
 
     async def create(
-            self,
-            session: AsyncSession,
-            id: int,
-            is_superuser: bool,
+        self,
+        is_superuser: bool,
+        user_id: int,
     ):
         admin = self.model(
-            id=id,
             is_superuser=is_superuser,
+            user_id=user_id,
         )
-        session.add(admin)
-        await session.commit()
-        await session.refresh(admin)
-        return admin
+        async with AsyncSessionLocal() as session:
+            session.add(admin)
+            await session.commit()
 
     async def get_all(self, session: AsyncSession):
-        return await session.execute(select(Admin)).scalars().all()
+        async with AsyncSessionLocal() as session:
+            admin_list = await session.execute(select(Admin))
+            admins = admin_list.scalars().all()
+            return admins
 
     async def remove(self, admin_obj, session: AsyncSession):
         if not admin_obj.is_superuser:
--- app/models/admin.py
+++ app/models/admin.py
@@ -1,6 +1,7 @@
 from app.core.db import Base
-from sqlalchemy import Boolean, Column
+from sqlalchemy import Boolean, Column, Integer
 
 
 class Admin(Base):
     is_superuser = Column(Boolean)
+    user_id = Column(Integer)
