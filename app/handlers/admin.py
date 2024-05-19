0737cb3890d3def828f0de0d9b6ce1a226bc1872
Pavel Ryabov <103175986+Devayter@users.noreply.github.com>
2024-05-19 10:19:15 +0300

    Update admin.py
    Исправил порядок импортов

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -2,9 +2,9 @@ from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
+
 from crud.admin import CRUDAdmin
 from keyboards.admin import admin_keyboard
-
 from .states import GetUserID
 
 router = Router()
